a32151236203722a93ecc240093e09169b2f855b
ShahilKM16 <mshahilk153@gmail.com>
2024-06-04 21:38:13 -1000

    renaming the incomeexpenses file

--- src/App.js
+++ src/App.js
@@ -1,48 +1,25 @@
-import React, { useState } from 'react';
-import { Header } from './components/Header';
-import { Balance } from './components/Balance';
-import { IncomeExpenses } from './components/IncomeExpenses';
-import { TransactionList } from './components/TransactionList';
-import { AddTransaction } from './components/AddTransaction';
+import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
-  const [transactions, setTransactions] = useState([
-    { id: 1, text: 'Cash', amount: 500 },
-    { id: 2, text: 'Book', amount: -40 },
-    { id: 3, text: 'Camera', amount: -200 },
-  ]);
-
-  const addTransaction = (transaction) => {
-    setTransactions([...transactions, transaction]);
-  };
-
-  const calculateBalance = () => {
-    const amounts = transactions.map(transaction => transaction.amount);
-    return amounts.reduce((acc, item) => (acc += item), 0);
-  };
-
-  const calculateIncome = () => {
-    const amounts = transactions.map(transaction => transaction.amount);
-    return amounts.filter(item => item > 0).reduce((acc, item) => (acc += item), 0);
-  };
-
-  const calculateExpense = () => {
-    const amounts = transactions.map(transaction => transaction.amount);
-    return amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1;
-  };
-
+function App() {
   return (
-    <div>
-      <Header />
-      <div className="container">
-        <Balance balance={calculateBalance()} />
-        <IncomeExpenses income={calculateIncome()} expense={calculateExpense()} />
-        <TransactionList transactions={transactions} />
-        <AddTransaction addTransaction={addTransaction} />
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
